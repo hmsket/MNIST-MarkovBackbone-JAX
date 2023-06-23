f94aafce40c091dcf5590729b4cfe7193d55529d
hmsket <74644437+hmsket@users.noreply.github.com>
2023-06-23 15:44:31 +0900

    add: 学習データと検証データで正解率を測定，学習したパラメータの保存

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import jax
 from jax import random, numpy as jnp
+
+import os
 from tqdm import tqdm
 
 from conv import Conv
@@ -79,6 +81,18 @@ for i in range(epochs):
     print(f'loss: {loss}')
 
 """ 検証 """
+# 学習データでの正解率測定
 y = predict(params, train_x)
-acc = F.test(y, train_t)
-print(f'acc: {acc}')
+train_acc = F.test(y, train_t)
+print(f'TRAIN_data_acc: {train_acc}')
+# 検証データでの正解率測定
+y = predict(params, test_x)
+test_acc = F.test(y, test_t)
+print(f'TEST_data_acc: {test_acc}')
+
+""" 学習済みパラメータの保存 """
+os.mkdir(f'./params/{num1}_{num2}_tr{train_acc:.3f}_te{test_acc:.3f}_nh{nh}_no{no}_s{seed}_m{mu}_e{epochs}_k{kernel_size[0]}_b{n_batch}')
+jnp.save(f'./params/{num1}_{num2}_tr{train_acc:.3f}_te{test_acc:.3f}_nh{nh}_no{no}_s{seed}_m{mu}_e{epochs}_k{kernel_size[0]}_b{n_batch}/conv_w', params[0])
+jnp.save(f'./params/{num1}_{num2}_tr{train_acc:.3f}_te{test_acc:.3f}_nh{nh}_no{no}_s{seed}_m{mu}_e{epochs}_k{kernel_size[0]}_b{n_batch}/conv_b', params[1])
+jnp.save(f'./params/{num1}_{num2}_tr{train_acc:.3f}_te{test_acc:.3f}_nh{nh}_no{no}_s{seed}_m{mu}_e{epochs}_k{kernel_size[0]}_b{n_batch}/out_w', params[2])
+jnp.save(f'./params/{num1}_{num2}_tr{train_acc:.3f}_te{test_acc:.3f}_nh{nh}_no{no}_s{seed}_m{mu}_e{epochs}_k{kernel_size[0]}_b{n_batch}/out_b', params[3])
