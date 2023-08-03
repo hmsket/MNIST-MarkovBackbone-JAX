5c8be5cb0f121917f91d11e107a473e0301554fd
hmsket <74644437+hmsket@users.noreply.github.com>
2023-08-03 21:05:58 +0900

    update: MNISTデータセットを，任意のラベルだけ取得できるようにした

--- model1/common.py
+++ model1/common.py
@@ -1,78 +1,54 @@
 from jax import numpy as jnp
+
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+
 from tensorflow.keras.datasets import mnist
+
 
 
 class F():
 
     def __init__(self):
         pass
-
-    def get_mnist_dataset(self):
-        (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-        train_images = train_images.astype('float32') / 255
-        test_images = test_images.astype('float32') / 255
-        train_labels = jnp.eye(10)[train_labels]
-        test_labels = jnp.eye(10)[test_labels]
-        return (train_images, train_labels), (test_images, test_labels)
-
-    def get_mnist_dataset_of_one_num(self, num):
-        (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-
-        train_images = train_images[jnp.where((train_labels == num))]
-        train_labels = train_labels[jnp.where((train_labels == num))]
-
-        test_images = test_images[jnp.where((test_labels == num))]
-        test_labels = test_labels[jnp.where((test_labels == num))]
-
-        train_labels = jnp.where(train_labels==num, 0, 1)
-        test_labels = jnp.where(test_labels==num, 0, 1)
-
-        train_labels = jnp.reshape(train_labels, [train_labels.shape[0], -1])
-        test_labels = jnp.reshape(test_labels, [test_labels.shape[0], -1])
-
-        # 0以上1以下に正規化
-        train_images = train_images.astype('float32') / 255
-        test_images = test_images.astype('float32') / 255
-        return (train_images, train_labels), (test_images, test_labels)
-
-    def get_mnist_dataset_of_two_nums(self, num1, num2):
-        (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-
-        train_images = train_images[jnp.where((train_labels == num1) | (train_labels == num2))]
-        train_labels = train_labels[jnp.where((train_labels == num1) | (train_labels == num2))]
-
-        test_images = test_images[jnp.where((test_labels == num1) | (test_labels == num2))]
-        test_labels = test_labels[jnp.where((test_labels == num1) | (test_labels == num2))]
-
-        train_labels = jnp.where(train_labels==num1, 0, 1)
-        test_labels = jnp.where(test_labels==num1, 0, 1)
-
-        # 出力層の素子が１個のとき
-        # train_labels = jnp.reshape(train_labels, [train_labels.shape[0], -1])
-        # test_labels = jnp.reshape(test_labels, [test_labels.shape[0], -1])
-
-        # 出力層の素子が２個以上のとき．（n個とする）
-        train_labels = jnp.eye(2)[train_labels]
-        test_labels = jnp.eye(2)[test_labels]
-
-        # 0以上1以下に正規化
-        train_images = train_images.astype('float32') / 255
-        test_images = test_images.astype('float32') / 255
-        return (train_images, train_labels), (test_images, test_labels)
-
-    def sigmoid(self, x):
-        return 1 / (1+jnp.exp(-x))
     
-    def tanh(self, x):
-        return (jnp.exp(x)-jnp.exp(-x)) / (jnp.exp(x)+jnp.exp(-x))
+    # MNISTデータセットのうち，リスト変数num_listで与えられたラベルのみ返す
+    def get_mnist_dataset(self, num_list):
+        (train_images_tmp, train_labels_tmp), (test_images_tmp, test_labels_tmp) = mnist.load_data()
+
+        train_idx = jnp.zeros(len(train_labels_tmp))
+        test_idx = jnp.zeros(len(test_labels_tmp))
+
+        for num in num_list:
+            train_idx = train_idx + (train_labels_tmp == num)
+            test_idx = test_idx + (test_labels_tmp == num)
+
+        train_images_tmp = train_images_tmp[jnp.where(train_idx)]
+        train_labels_tmp = train_labels_tmp[jnp.where(train_idx)]
+
+        test_images_tmp = test_images_tmp[jnp.where(test_idx)]
+        test_labels_tmp = test_labels_tmp[jnp.where(test_idx)]
+
+        # 画像のピクセル値を，0以上1以下に正規化
+        train_images = train_images_tmp.astype('float32') / 255
+        test_images = test_images_tmp.astype('float32') / 255
+
+        # ラベルのone-hotベクトル化
+        train_labels = jnp.zeros(len(train_labels_tmp))
+        test_labels = jnp.zeros(len(test_labels_tmp))
+        for i in range(len(num_list)):
+            train_labels = jnp.where(train_labels_tmp==num_list[i], i, train_labels)
+            test_labels = jnp.where(test_labels_tmp==num_list[i], i, test_labels)
+        train_labels = jnp.eye(len(num_list))[jnp.int8(train_labels)]
+        test_labels = jnp.eye(len(num_list))[jnp.int8(test_labels)]
+
+        return (train_images, train_labels), (test_images, test_labels)
 
     def softmax(self, x, axis):
         max = jnp.max(x, axis=axis, keepdims=True)
         exp_x = jnp.exp(x-max)
         sum_exp_x = jnp.sum(exp_x, axis=axis, keepdims=True)
-        new_x = exp_x / sum_exp_x
+        new_x = jnp.divide(exp_x, sum_exp_x)
         return new_x
     
     def plot_area_of_max_weighted_sum(self, conv, params, image):
