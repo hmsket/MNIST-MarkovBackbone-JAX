d2c145e82b89e5940e7dccd387ddee522cf04d35
hmsket <74644437+hmsket@users.noreply.github.com>
2023-10-27 18:20:07 +0900

    update: クラスじゃなくて，ただの関数にした

--- model1/common.py
+++ model1/common.py
@@ -1,98 +1,65 @@
+import numpy as np
 from jax import numpy as jnp
 
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-
 from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
 
 
 
-class F():
+# MNISTのうち，配列labelsで指定したラベルのみのデータセットを作成する関数
+def get_mnist_dataset(labels):
+    (train_x, train_y), (test_x, test_y) = mnist.load_data()
 
-    def __init__(self):
-        pass
-    
-    # MNISTデータセットのうち，リスト変数num_listで与えられたラベルのみ返す
-    def get_mnist_dataset(self, num_list):
-        (train_images_tmp, train_labels_tmp), (test_images_tmp, test_labels_tmp) = mnist.load_data()
+    train_idx = jnp.zeros(len(train_y))
+    test_idx = jnp.zeros(len(test_y))
 
-        train_idx = jnp.zeros(len(train_labels_tmp))
-        test_idx = jnp.zeros(len(test_labels_tmp))
+    for label in labels:
+        train_idx = train_idx + (train_y == label)
+        test_idx = test_idx + (test_y == label)
 
-        for num in num_list:
-            train_idx = train_idx + (train_labels_tmp == num)
-            test_idx = test_idx + (test_labels_tmp == num)
+    train_x = train_x[jnp.where(train_idx)]
+    train_y = train_y[jnp.where(train_idx)]
 
-        train_images_tmp = train_images_tmp[jnp.where(train_idx)]
-        train_labels_tmp = train_labels_tmp[jnp.where(train_idx)]
+    test_x = test_x[jnp.where(test_idx)]
+    test_y = test_y[jnp.where(test_idx)]
 
-        test_images_tmp = test_images_tmp[jnp.where(test_idx)]
-        test_labels_tmp = test_labels_tmp[jnp.where(test_idx)]
+    # ラベルの値を変える
+    # e.g. labels=[6,7]のとき，6を0,7を1に変える
+    # こうしないと，to_categorical()が使えない
+    for i, label in enumerate(labels):
+        np.place(train_y, (train_y==label)>0, i)
+        np.place(test_y, (test_y==label)>0, i)
+ 
+    # 画像のピクセル値を，0以上1以下に正規化
+    train_x = train_x.astype('float32') / 255
+    test_x = test_x.astype('float32') / 255
 
-        # 画像のピクセル値を，0以上1以下に正規化
-        train_images = train_images_tmp.astype('float32') / 255
-        test_images = test_images_tmp.astype('float32') / 255
+    # 望ましい値を，one-hotベクトルにする
+    N = len(labels)
+    train_y = to_categorical(train_y, N) # ２番目の引数で，次元数を指定する
+    test_y = to_categorical(test_y, N)
 
-        # ラベルのone-hotベクトル化
-        train_labels = jnp.zeros(len(train_labels_tmp))
-        test_labels = jnp.zeros(len(test_labels_tmp))
-        for i in range(len(num_list)):
-            train_labels = jnp.where(train_labels_tmp==num_list[i], i, train_labels)
-            test_labels = jnp.where(test_labels_tmp==num_list[i], i, test_labels)
-        train_labels = jnp.eye(len(num_list))[jnp.int8(train_labels)]
-        test_labels = jnp.eye(len(num_list))[jnp.int8(test_labels)]
+    # numpyの配列を，jax.numpyの配列に型変換する
+    train_x = jnp.array(train_x)
+    train_y = jnp.array(train_y)
+    test_x = jnp.array(test_x)
+    test_y = jnp.array(test_y)
 
-        train_images = jnp.array(train_images)
-        train_labels = jnp.array(train_labels)
-        test_images = jnp.array(test_images)
-        test_labels = jnp.array(test_labels)
+    return (train_x, train_y), (test_x, test_y)
 
-        return (train_images, train_labels), (test_images, test_labels)
 
-    def softmax(self, x, t, axis):
-        max = jnp.max(x, axis=axis, keepdims=True)
-        exp_x = jnp.exp((x-max)/t)
-        sum_exp_x = jnp.sum(exp_x, axis=axis, keepdims=True)
-        new_x = jnp.divide(exp_x, sum_exp_x)
-        return new_x
-    
-    def plot_area_of_max_weighted_sum(self, conv, params, image):
-        conv_w, conv_b = params
-        tmp_image = jnp.reshape(image, [1, image.shape[0], image.shape[1]]) # 自作関数を使うための，次元のつじつま合わせ
-        hidden = conv.forward(conv_w, conv_b, tmp_image)
-        tmp_idxs = jnp.argmax(hidden, axis=2)
-        idxs = jnp.reshape(tmp_idxs, tmp_idxs.shape[1]) # ２次元から１次元にする
-        
-        colors = ['r', 'g', 'b', 'c', 'm'] # 領域を示す線の色を適当に５色用意する
+def softmax(x, axis, t=1.0):
+    max = jnp.max(x, axis=axis, keepdims=True)
+    exp_x = jnp.exp((x-max)/t)
+    sum_exp_x = jnp.sum(exp_x, axis=axis, keepdims=True)
+    new_x = jnp.divide(exp_x, sum_exp_x)
+    return new_x
 
-        ax = plt.axes()
-        ax.set_xticks([])
-        ax.set_yticks([])
 
-        for i in range(len(idxs)):
-            idx = idxs[i]
-            # 畳み込み後の行列の添字に対応する，畳み込み前の行列の添字を計算
-            q = idx // (image.shape[0] - conv.kernel_size[0] + 1)
-            tmp_idx = idx + q * (conv.kernel_size[0] - 1)
-            # ベクトルの添字に対応する，行列にreshapeしたときの座標を計算
-            x = tmp_idx // image.shape[0]
-            y = tmp_idx % image.shape[0]
-            r = patches.Rectangle(xy=(y-0.5,x-0.5), width=conv.kernel_size[0], height=conv.kernel_size[1], fill=False, color=colors[i%5])
-            ax.add_patch(r)
-
-        ax.imshow(image, cmap=plt.cm.gray_r)
-        plt.show()
-    
-    def test(self, y, t):
-        total = t.shape[0]
-
-        # 出力層の素子が１個のとき
-        # y = jnp.where(y < 0, 0, 1)
-        
-        # 出力層の素子が２個以上のとき
-        y = jnp.argmax(y, axis=1)
-        t = jnp.argmax(t, axis=1)
-        
-        ans = jnp.where(y == t, 1, 0)
-        acc = jnp.sum(ans) / total
-        return acc
+def test(z, y):
+    N = len(y)
+    z = jnp.argmax(z, axis=1)
+    y = jnp.argmax(y, axis=1)
+    ans = jnp.where(z==y, 1, 0)
+    acc = jnp.sum(ans) / N
+    return acc
